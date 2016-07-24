1a58254be0f7208e2f7a989eafb0c41def7d659a
Travis CI <travis@localhost.localdomain>
2016-07-23 23:33:34 -0500

    Publish tagged commit

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
-cd dist
-git init
-git config --global user.email "travis@localhost.localdomain"
-git config --global user.name "Travis CI"
-git add --all
-git commit -am "Travis change"
+if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    cd dist
+    git init
+    git config --global user.email "travis@localhost.localdomain"
+    git config --global user.name "Travis CI"
+    git add --all
+    git commit -am "Deploy version $TRAVIS_TAG"
+else
+  echo "This will not deploy!"
+fi
